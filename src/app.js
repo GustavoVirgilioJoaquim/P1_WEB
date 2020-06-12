b363c61779d86ebb08300236990b43dfe821640b
Gugu <43094715+GustavoVirgilioJoaquim@users.noreply.github.com>
2020-06-12 03:03:13 -0300

    update 1.2

--- src/app.js
+++ src/app.js
@@ -8,4 +8,15 @@ app.get('/', (request, response) => {
     response.sendFile(__dirname + '/static/html/index.html')
 })
 
+app.get('/sobre', (request, response) => {
+    response.sendFile(__dirname + '/static/html/sobre.html')
+})
+
+app.get('/trabalhos', (request, response) => {
+    response.sendFile(__dirname + '/static/html/trabalhos.html')
+})
+
+app.get('/contatos', (request, response) => {
+    response.sendFile(__dirname + '/static/html/contatos.html')
+})
 app.listen(port)
