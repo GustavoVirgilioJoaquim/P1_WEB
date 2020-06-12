02d7fcb5411a51596a7ac7ae45e0e4a44d8f3645
Gugu <43094715+GustavoVirgilioJoaquim@users.noreply.github.com>
2020-06-12 02:32:13 -0300

    update 1.0

---
+++ src/app.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3333
+
+app.use(express.static(__dirname + '/static/'))
+
+app.get('/', (request, response) => {
+    response.sendFile(__dirname + '/static/HTML/index.html')
+})
+
+app.listen(port)
